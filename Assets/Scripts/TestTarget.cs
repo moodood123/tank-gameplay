6ee3137a30df6c1b4c0e6999157723f8b257c714
Sam de Paiva <sammydepaiva@gmail.com>
2025-10-02 22:58:12 -0600

    Imported basic player animations

--- Assets/Scripts/TestTarget.cs
+++ Assets/Scripts/TestTarget.cs
@@ -1,8 +1,15 @@
+using System.Collections;
+using PrimeTween;
 using UnityEngine;
 
 public class TestTarget : MonoBehaviour, IDamageable
 {
     [SerializeField] private float _maxHealth;
+    [SerializeField] private Transform _mesh;
+    
+    [Header("Animation Settings")] 
+    [SerializeField] private TweenSettings<Vector3> _deflateSettings;
+    [SerializeField] private TweenSettings<Vector3> _inflateSettings;
     
     public float Health { get; private set; }
 
@@ -12,15 +19,23 @@ public class TestTarget : MonoBehaviour, IDamageable
     {
         Health = _maxHealth;
     }
-    
+
     public void TakeDamage(float damage)
     {
+        if (!IsAlive) return;
         Health -= damage;
         if (!IsAlive) Die();
     }
 
     private void Die()
     {
-        Destroy(this.gameObject);
+        StartCoroutine(RegenSequence());
+    }
+
+    private IEnumerator RegenSequence()
+    {
+        yield return Tween.Scale(_mesh, _deflateSettings).ToYieldInstruction();
+        yield return Tween.Scale(_mesh, _inflateSettings).ToYieldInstruction();
+        Health = _maxHealth;
     }
 }
