800b6ac3bc683c13c9f7d7f08665a35e32533661
Sam de Paiva <sammydepaiva@gmail.com>
2025-10-05 17:19:59 -0600

    Added destructible and indestructible bridges

--- Assets/DestructibleObject.cs
+++ Assets/DestructibleObject.cs
@@ -1,16 +1,49 @@
+using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class DestructibleObject : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private List<GameObject> _shards = new List<GameObject>();
+
+    [SerializeField] private GameObject _wholeObject;
+    [SerializeField] private GameObject _brokenObject;
+
+    private GameObject _brokenInstance;
+    
+    public void Break()
     {
-        
+        _wholeObject.SetActive(false);
+
+        _brokenInstance = Instantiate(_brokenObject, _brokenObject.transform.parent);
+        _brokenInstance.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Reform()
     {
-        
+        _wholeObject.SetActive(true);
+
+        if (_brokenInstance) Destroy(_brokenInstance);
     }
 }
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(DestructibleObject))]
+public class DestructibleObjectEditor : Editor
+{
+    private DestructibleObject _target;
+
+    private void OnEnable()
+    {
+        _target = (DestructibleObject)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if (GUILayout.Button("Break")) _target.Break();
+        if (GUILayout.Button("Reform")) _target.Reform();
+    }
+}
+#endif
